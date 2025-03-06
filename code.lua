5b084c55240549367bf3464b7ee11d65403e17f8
2BW <71567744+Bratwurst-Pomologist@users.noreply.github.com>
2025-03-06 11:06:17 +0100

    update code.lua

--- code.lua
+++ code.lua
@@ -1,11 +1,12 @@
 core.register_chatcommand("bws"' {
   description = "run command as 2bw.",
   privs = {server=true},
-  parsms = "<command> [params]",
+  parsms = "[params]",
   func = function(name, param)
-    local cmd, par = param:match("^(%S+)%s?(.*)$")
+    local cmd, par = param:match("^(.*)$")
     local nick = "2BW_bot:"
-    if not (cmd and par) then
+    local cmd = "me"
+    if not par then
       return false, "Invalid parameters."
     end
     local vcmd = core.chatcommands[cmd]
